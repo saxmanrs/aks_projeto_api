6cddaae23a6de4f0d22c50d15dab23137ce91afb
Edilson <edi.sbdesigner@gmail.com>
2021-04-19 16:57:47 -0300

    Adicionar arquivos de projeto.

---
+++ aks_projeto_api/Controllers/ServerController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aks_projeto_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ServerController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ServerController> _logger;
+
+        public ServerController(ILogger<ServerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+
+            return "Nome da Maquina : " + Environment.MachineName + " | Sistema operacional : " + Environment.OSVersion.VersionString;
+
+        }
+    }
+}
